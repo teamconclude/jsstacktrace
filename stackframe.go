3e97963f865a9f59fc02d8eda270f8f90076b39a
Lars Knoll <lars@knoll.priv.no>
2025-01-23 11:31:45 +0100

    More fixes for frame parsing

--- stackframe.go
+++ stackframe.go
@@ -1,8 +1,10 @@
 package jsstacktrace
 
 import (
+	"log"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 type StackFrame struct {
@@ -13,53 +15,88 @@ type StackFrame struct {
 }
 
 func (sf *StackFrame) String() string {
-	return "at " + sf.Function + " (" + sf.Url + ":" + strconv.Itoa(sf.Line) + ":" + strconv.Itoa(sf.Column) + ")"
+	result := "at " + sf.Function + " (" + sf.Url
+	if sf.Line >= 0 {
+		result += ":" + strconv.Itoa(sf.Line)
+		if sf.Column >= 0 {
+			result += ":" + strconv.Itoa(sf.Column)
+		}
+	}
+	return result + ")"
 }
 
 // Stack trace formats:
 // Safari and Firefox: "function@url:line:column"
 // Chrome and Edge: "\s+at function (url:line:column)"
 
-var stackFrameChrome = regexp.MustCompile(`\s*at\s+(?P<function>[^\s]+)\s+\(\s*(?P<file>[^\s]+)\s*\)\s*`)
-var stackFrameSafari = regexp.MustCompile(`\s*(?P<function>[^@\s]+)@(?P<file>[^\s]+)\s*`)
+var chromeFormat = regexp.MustCompile(`\s*at\s+(.*)`)
+var safariFormat = regexp.MustCompile(`\s*(.*)@(.*)`)
 
-var splitLocationWithColumn = regexp.MustCompile(`(?P<url>.+):(?P<line>\d+):(?P<column>\d+)`)
-var splitLocationWithoutColumn = regexp.MustCompile(`(?P<url>.+):(?P<line>\d+)`)
+var stackFrameChrome = regexp.MustCompile(`([^( ]+)\s+\(\s*(?P<file>[^\s]+)\s*\)\s*`)
+
+var splitLocationWithColumn = regexp.MustCompile(`(?P<url>.+):(?P<line>\d+):(?P<column>\d+)\s*`)
+var splitLocationWithoutColumn = regexp.MustCompile(`(?P<url>.+):(?P<line>\d+)\s*`)
+
+func parseLocation(location string) (string, int, int) {
+	url := location
+	line := -1
+	column := -1
+
+	if splitLocationWithColumn.MatchString(location) {
+		matches := splitLocationWithColumn.FindStringSubmatch(location)
+		url = matches[1]
+		line, _ = strconv.Atoi(matches[2])
+		column, _ = strconv.Atoi(matches[3])
+	} else if splitLocationWithoutColumn.MatchString(location) {
+		matches := splitLocationWithoutColumn.FindStringSubmatch(location)
+		url = matches[1]
+		line, _ = strconv.Atoi(matches[2])
+	}
+
+	return url, line, column
+}
+
+func parseFrameChrome(s string) *StackFrame {
+	log.Default().Println("parseFrameChrome: ", s)
+	matches := stackFrameChrome.FindStringSubmatch(s)
+	loc := s
+	function := ""
+	if len(matches) == 0 {
+		if strings.Contains(s, ":") {
+			loc = s
+		} else {
+			function = s
+			loc = ""
+		}
+	} else {
+		function = matches[1]
+		loc = matches[2]
+	}
+
+	url, line, column := parseLocation(loc)
+
+	return &StackFrame{Function: function, Url: url, Line: line, Column: column}
+}
+
+func parseFrameSafari(f string, l string) *StackFrame {
+	function := f
+	url, line, column := parseLocation(l)
+
+	return &StackFrame{Function: function, Url: url, Line: line, Column: column}
+}
 
 func StackFrameFromString(s string) *StackFrame {
 	// check which format the stack trace has
-	var matches []string
-	if stackFrameSafari.MatchString(s) {
-		matches = stackFrameSafari.FindStringSubmatch(s)
-	} else if stackFrameChrome.MatchString(s) {
-		matches = stackFrameChrome.FindStringSubmatch(s)
+	log.Default().Println("StackFrameFromString: ", s)
+	if chromeFormat.MatchString(s) {
+		log.Default().Println("StackFrameFromString: chromeFormat")
+		matches := chromeFormat.FindStringSubmatch(s)
+		return parseFrameChrome(matches[1])
+	} else if safariFormat.MatchString(s) {
+		log.Default().Println("StackFrameFromString: safariFormat")
+		matches := safariFormat.FindStringSubmatch(s)
+		return parseFrameSafari(matches[1], matches[2])
 	} else {
-		return &StackFrame{Function: "JSStackframe: not converted: ", Url: s, Line: 0, Column: 0}
+		return nil
 	}
-	function := matches[1]
-	location := matches[2]
-	var url string
-	var line int
-	var column int
-
-	var err error
-	if splitLocationWithColumn.MatchString(location) {
-		matches = splitLocationWithColumn.FindStringSubmatch(location)
-		url = matches[1]
-		column, err = strconv.Atoi(matches[3])
-		if err == nil {
-			line, err = strconv.Atoi(matches[2])
-		}
-	}
-	if (url == "" || err != nil) && splitLocationWithoutColumn.MatchString(location) {
-		matches = splitLocationWithoutColumn.FindStringSubmatch(location)
-		url = matches[1]
-		line, err = strconv.Atoi(matches[2])
-		column = 0
-	}
-	if url == "" || err != nil {
-		url = location
-	}
-
-	return &StackFrame{Function: function, Url: url, Line: line, Column: column}
 }
--- stackframe_test.go
+++ stackframe_test.go
@@ -42,18 +42,49 @@ func TestStackFrameFromString(t *testing.T) {
 				Function: ".LT",
 				Url:      "http://localhost:8080/dist/js/chunk-M6AOQWLO.js",
 				Line:     12217,
-				Column:   0,
+				Column:   -1,
 			},
+		},
+		{
+			input: "Error",
+			want:  nil,
+		},
+		{
+			input: "Error: something went wrong",
+			want:  nil,
+		},
+		{
+			input: "   at <anonymous>",
+			want:  &StackFrame{Function: "<anonymous>", Url: "", Line: -1, Column: -1},
+		},
+		{
+			input: "   at http://localhost:8080/dist/js/chunk-M6AOQWLO.js:12217:35",
+			want:  &StackFrame{Function: "", Url: "http://localhost:8080/dist/js/chunk-M6AOQWLO.js", Line: 12217, Column: 35},
+		},
+		{
+			input: "@http://path/to/file.js:48",
+			want:  &StackFrame{Function: "", Url: "http://path/to/file.js", Line: 48, Column: -1},
+		},
+		{
+			input: "printStackTrace()@file:///G:/js/stacktrace.js:18",
+			want:  &StackFrame{Function: "printStackTrace()", Url: "file:///G:/js/stacktrace.js", Line: 18, Column: -1},
+		},
+		{
+			input: ".plugin/e.fn[c]/<@http://path/to/file.js:1:1",
+			want:  &StackFrame{Function: ".plugin/e.fn[c]/<", Url: "http://path/to/file.js", Line: 1, Column: 1},
 		},
 	}
 
 	for _, tt := range tests {
 		got := StackFrameFromString(tt.input)
-		if got == nil {
-			t.Fatalf("StackFrameFromString(%q) = nil, want %v", tt.input, tt.want)
-		}
-		if *got != *tt.want {
-			t.Errorf("StackFrameFromString(%q) = %v, want %v", tt.input, got, tt.want)
+		if got != nil || tt.want != nil {
+			if got == nil {
+				t.Fatalf("StackFrameFromString(%q) = nil, want %v", tt.input, tt.want)
+			} else if tt.want == nil {
+				t.Fatalf("StackFrameFromString(%q) = %v, want nil", tt.input, got)
+			} else if *got != *tt.want {
+				t.Errorf("StackFrameFromString(%q) = %v, want %v", tt.input, got, tt.want)
+			}
 		}
 	}
 }
